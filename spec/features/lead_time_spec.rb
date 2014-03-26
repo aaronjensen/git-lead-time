e068dd26cb4242fde5a15a228175a4b158967cdc
Aaron Jensen <aaronjensen@gmail.com>
2014-03-26 09:03:20 -0700

    Set up scenario for lead time

--- spec/features/lead_time_spec.rb
+++ spec/features/lead_time_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe "git-lead-time", :git do
   it "shows the lead time for each merge" do
+    git_commit on("monday 9am")
+    git :checkout, "-b", "topic"
+    git_commit on("tuesday 9am")
+    git :checkout, "master"
+    git_merge "topic", on("wednesday 9am")
+
+    git "log", "--graph"
     git "lead-time"
     skip
   end
--- spec/support/git.rb
+++ spec/support/git.rb
@@ -1,13 +1,40 @@
 require 'tmpdir'
+require 'chronic'
 
 module Support
   module Git
+    def git_commit(message = :default, date:)
+      @commit_counter ||= 0
+      @commit_counter += 1
+      args = []
+      case message
+      when :default
+        args += ["-m", "Commit ##{@commit_counter}"]
+      when :no_edit
+        args += ["--no-edit"]
+      else
+        args += ["-m", message]
+      end
+
+      git :commit, "--allow-empty", "--date", date, "-m", message, date: date
+    end
+
+    def git_merge(branch, date:)
+       git :merge, "--no-ff", "--no-commit", "topic", date: date
+       git_commit :no_edit, date: date
+    end
+
     def git(*args, date: :now)
       env = {}
       if date != :now
         env["GIT_COMMITTER_DATE"] = date
       end
+      p date
       raise unless system(env, "git", *args.map(&:to_s))
+    end
+
+    def on(date)
+      { date: Chronic.parse(date, now: Time.local(2014, 2, 1)).strftime("%a, %b %d %k:%M %Y %z") }
     end
   end
 end
@@ -16,7 +43,7 @@ RSpec.configure do |config|
   config.around :each, :git do |example|
     Dir.mktmpdir do |dir|
       Dir.chdir(dir) do
-        git "init"
+        git :init
         example.run
       end
     end
